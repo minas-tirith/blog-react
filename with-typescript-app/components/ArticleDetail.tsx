e21aa90d0f072067d4cf85d4dfe7a04d1aae8ad2
Antoine Grandiere <antoinegrandiere@MacBook-Pro-de-Antoine.local>
2021-05-10 15:56:17 +0200

    feat: add redux

--- with-typescript-app/components/ArticleDetail.tsx
+++ with-typescript-app/components/ArticleDetail.tsx
@@ -1,16 +1,27 @@
 import * as React from 'react'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { incrementCount, decrementCount, resetCount } from '../actions'
 import { Article } from '../interfaces'
 
 type ArticleDetailProps = {
   item: Article
 }
 
-const ListDetail = ({ item: article }: ArticleDetailProps) => (
-  <div>
-    <h1>Title: {article.title}</h1>
-    <p>ID: {article.id}</p>
-  </div>
-)
+const ListDetail = ({ item: article }: ArticleDetailProps) => {
+  console.log(useDispatch)
+  const dispatch = useDispatch()
+  const state = useSelector((state) => state)
+
+  return (
+    <div>
+      <h1>Title: {article.title}</h1>
+      <p>ID: {article.id}</p>
+      <button onClick={() => dispatch(incrementCount())}>+1</button>
+      <button onClick={() => dispatch(decrementCount())}>-1</button>
+      <button onClick={() => dispatch(resetCount())}>Reset</button>
+      <code>{JSON.stringify(state, null, 4)}</code>
+    </div>
+  )
+}
 
 export default ListDetail
--- with-typescript-app/pages/index.tsx
+++ with-typescript-app/pages/index.tsx
@@ -1,15 +1,20 @@
 import Link from 'next/link'
+import * as React from 'react'
+import { Provider } from 'react-redux'
+import { useStore } from '../store'
 import Layout from '../components/Layout'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/articles">
-        <a>Articles</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <>
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href="/articles">
+          <a>Articles</a>
+        </Link>
+      </p>
+    <>
+  )
+}
 
-export default IndexPage
+export default IndexPage;
